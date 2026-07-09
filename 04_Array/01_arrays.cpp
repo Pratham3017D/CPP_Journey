c82747d55954a476d773d62d20336bc7b1abe903
Pratham Rathore <prathampipliya1234@gmail.com>
2026-07-09 23:13:11 +0530

    This is start of Arrays

--- 04_Array/01_arrays.cpp
+++ 04_Array/01_arrays.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main(){
+    // int dog[15];
+
+    // cout << dog[5] << endl;
+
+    // int second[3] = {5, 7, 11};
+    // cout << endl << second[2] << endl;
+
+    int n;
+    cin >> n;
+    
+    int numbers[50] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
+    cout << numbers[n-1] << endl;
     
 }
