73a184e1427abad8ba64335b5186bf22c272190e
Pratham Rathore <prathampipliya1234@gmail.com>
2026-07-13 20:13:39 +0530

    Reverse without Swap

--- 04_Array/03_reverseArray.cpp
+++ 04_Array/03_reverseArray.cpp
@@ -32,3 +32,36 @@ int main(){
 
 
 }
+
+
+// void reverse(int arr[], int size){
+
+//     for (int i = 0; i < size/2; i++){
+//         int store = arr[i];
+//         arr[i] = arr[size -1 - i];
+//         arr[size -1 - i] = store;
+//     }
+    
+    
+// }
+
+// void printArray(int arr[], int n){
+
+//     for (int i= 0; i<n; i++){
+//         cout << arr[i]<< " ";
+//     }
+//     cout << endl;
+// }
+
+// int main(){
+//     int arr[6] = { 1, 2, 3, 4, 5, 6 };
+//     int brr[5] = { 5, 4, 3, 2, 1 };
+
+//     reverse(arr , 6);
+//     reverse(brr, 5);
+
+//     printArray(arr, 6);
+//     printArray(brr, 5);
+
+
+// }
